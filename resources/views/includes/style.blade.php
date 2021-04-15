4907c1f7b872db59848e11326d0933c75bfe7570
ryanspandu <1910036@gmail.com>
2021-04-15 17:12:08 +0700

    favicon added

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -6,5 +6,5 @@
         crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw==" 
                 crossorigin="anonymous" />
-
+<link rel="icon" href="{{ asset('images/favicon.svg') }}" type="image/gif" sizes="16x16">
 <link rel="stylesheet" href="{{ asset('css/color.css') }}"/>
