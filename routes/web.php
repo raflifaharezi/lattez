dcd2fbdfc66053210aba4633a983eef6c27c2776
ryanspandu <1910036@gmail.com>
2021-04-16 10:15:07 +0700

    auth route

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('sales');
 });
-
-Route::get('/sales', 'Lattez\SalesandBonusController@index')->name('sales');
-Route::get('/transaction-history','Lattez\TransactionHistoryController@index')->name('transaction');
-Route::get('/bonus-statement', 'Lattez\BonusStatementController@index')->name('bonus-statement');
+Route::group(['middleware' => ['auth']],function (){
+    Route::get('/sales', 'Lattez\SalesandBonusController@index')->name('sales');
+    Route::get('/transaction-history','Lattez\TransactionHistoryController@index')->name('transaction');
+    Route::get('/bonus-statement', 'Lattez\BonusStatementController@index')->name('bonus-statement');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
