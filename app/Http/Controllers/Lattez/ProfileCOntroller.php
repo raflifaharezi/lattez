92ddd1886714f9cec564a050e1168d8ec4546b99
raflifaharezi <raflifahrezi30@gmail.com>
2021-04-16 10:16:13 +0700

    on progress

---
+++ app/Http/Controllers/Lattez/ProfileCOntroller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Lattez;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileCOntroller extends Controller
+{
+    //
+}
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link text-white" href="#">Profile <span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-white" href="{{ route('profile') }}">Profile <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="{{ route('transaction') }}">History Transaksi</a>
